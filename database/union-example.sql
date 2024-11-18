43af06fbfd216aa4e66cc243d20765119f644f02
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-11-17 21:04:16 -0800

    Add comments to clarify usage of UNION ALL in SQL query

--- database/union-example.sql
+++ database/union-example.sql
@@ -4,6 +4,9 @@
 -- The columns in the first query are used to determine the name of the columns in the result set
 -- The columns in the second query are ignored
 
+-- Union all joins the results of two or more queries together
+-- union all does not remove duplicate rows
+
 
 (
   SELECT *
