7e29dbf9c7fe8b3d4ba6f5ad349cc2e5556566c8
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-11-19 06:20:51 -0800

    Add subquery examples in SELECT clause with explanations

---
+++ database/subquery-exercises/select-clause.sql
@@ -0,0 +1,28 @@
+--Subject: Subquery examples
+-- Subquery in SELECT clause
+-- Must return a single value
+-- SELECT name,
+--   price,
+--   (
+--     SELECT MAX(price) <==== Subquery that returns a single value
+--     FROM products     <==== 
+--   )
+-- FROM products
+-- WHERE price > 867;
+-- SELECT name,
+--   price,
+--   (
+--     SELECT price
+--     FROM products
+--     WHERE id = 3
+--   ) AS id_3_price
+-- FROM products
+-- WHERE price > 867;
+
+SELECT name,
+  price,
+  price::FLOAT / (
+    SELECT MAX(price)
+    FROM products
+  ) AS max_price
+FROM products;
