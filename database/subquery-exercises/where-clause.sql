9ce06fd404c567596493390c1969a1c485707bbf
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-11-20 06:03:40 -0800

    Enhance subquery examples in WHERE clause with additional queries and explanations

--- database/subquery-exercises/where-clause.sql
+++ database/subquery-exercises/where-clause.sql
@@ -3,8 +3,14 @@
 -- depending on the operator used in the WHERE clause ex: >, <, IN, NOT IN, etc.
 -- Most the time you can solve the same problem with a JOIN clause
 -- But sometimes a subquery in the WHERE clause is more readable
+--
+--
+--
 
--- The subquery in the WHERE clause is executed first
+
+
+
+-- Subquery in WHERE clause returns a single column ---------------------------------------------
 SELECT id
 FROM orders
 WHERE product_id IN (
@@ -12,9 +18,28 @@ WHERE product_id IN (
     FROM products
     WHERE price / weight > 50
   );
-
--- The same query using a JOIN clause
+-- The same query using a JOIN clause -----------------------------------
 SELECT orders.id AS order_id
 FROM orders
   JOIN products ON products.id = orders.product_id
 WHERE products.price / products.weight > 50;
+
+
+
+
+
+-- Subquery in WHERE clause returns a single value ---------------------------------------------
+SELECT name
+FROM products
+WHERE price > (
+    SELECT AVG(price)
+    FROM products
+  );
+
+-- The same query using a JOIN clause -----------------------------------
+SELECT name
+FROM products
+  JOIN (
+    SELECT AVG(price) AS avg_price
+    FROM products
+  ) as p ON products.price > p.avg_price;
