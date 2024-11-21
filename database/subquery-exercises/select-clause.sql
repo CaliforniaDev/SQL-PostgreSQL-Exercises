fcd3b632dba33e6ea5171402c6a49f56d9e8e03a
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-11-21 06:30:57 -0800

    Refactor subquery examples in SELECT clause for clarity and consistency

--- database/subquery-exercises/select-clause.sql
+++ database/subquery-exercises/select-clause.sql
@@ -1,28 +1,51 @@
---Subject: Subquery examples
--- Subquery in SELECT clause
--- Must return a single value
--- SELECT name,
---   price,
---   (
---     SELECT MAX(price) <==== Subquery that returns a single value
---     FROM products     <==== 
---   )
--- FROM products
--- WHERE price > 867;
--- SELECT name,
---   price,
---   (
---     SELECT price
---     FROM products
---     WHERE id = 3
---   ) AS id_3_price
--- FROM products
--- WHERE price > 867;
+/*
+Subqueries in SELECT clause
+the subquery return a single value
+ */
+SELECT
+  name,
+  price,
+  (
+    SELECT
+      MAX(price) --- Subquery that returns a single value
+    FROM
+      products
+  )
+FROM
+  products
+WHERE
+  price > 867;
 
-SELECT name,
+-------------------------------------------
+--
+--
+SELECT
+  name,
+  price,
+  (
+    SELECT
+      price
+    FROM
+      products
+    WHERE
+      id = 3
+  ) AS id_3_price
+FROM
+  products
+WHERE
+  price > 867;
+
+-------------------------------------------
+--
+--
+SELECT
+  name,
   price,
   price::FLOAT / (
-    SELECT MAX(price)
-    FROM products
+    SELECT
+      MAX(price)
+    FROM
+      products
   ) AS max_price
-FROM products;
+FROM
+  products;
