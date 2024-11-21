ed67ab1e3454066e9fa64df661f3b11977ecdbfa
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-11-21 06:36:20 -0800

    Refactor subquery examples in FROM clause for improved readability and consistency

--- database/subquery-exercises/from-clause.sql
+++ database/subquery-exercises/from-clause.sql
@@ -4,25 +4,40 @@
 -- The subquery must have an alias
 -- The alias is used to reference the subquery in the FROM clause
 -- Any subquery as long the select/from/where clauses are compatible
--- SELECT name,
---   price_weight_ratio
--- FROM (
---     SELECT name,
---       price / weight AS price_weight_ratio
---     FROM products
---   ) AS p
--- WHERE price_weight_ratio > 200;
--- SELECT max_price
--- FROM (
---     SELECT MAX(price)
---     FROM products
---   ) AS max_price;
 
+SELECT
+  name,
+  price_weight_ratio
+FROM
+  (
+    SELECT
+      name,
+      price / weight AS price_weight_ratio
+    FROM
+      products
+  ) AS p
+WHERE
+  price_weight_ratio > 200;
 
-SELECT AVG(order_count)
-FROM (
-    SELECT user_id,
+SELECT
+  max_price
+FROM
+  (
+    SELECT
+      MAX(price)
+    FROM
+      products
+  ) AS max_price;
+
+SELECT
+  AVG(order_count)
+FROM
+  (
+    SELECT
+      user_id,
       COUNT(*) AS order_count
-    FROM orders
-    GROUP BY user_id
+    FROM
+      orders
+    GROUP BY
+      user_id
   ) AS user_order_counts;
