cdeb8b034564c2fe42d41097df81bd677391c533
Leo <37211774+CaliforniaDev@users.noreply.github.com>
2024-11-21 05:51:00 -0800

    Enhance subquery examples in WHERE clause with additional queries and explanations

--- database/subquery-exercises/where-clause.sql
+++ database/subquery-exercises/where-clause.sql
@@ -1,45 +1,91 @@
--- Subquery in WHERE clause
--- Structure of data allowed to be returned by subquery 
--- depending on the operator used in the WHERE clause ex: >, <, IN, NOT IN, etc.
--- Most the time you can solve the same problem with a JOIN clause
--- But sometimes a subquery in the WHERE clause is more readable
+/*
+Subquery in WHERE clause
+- Structure of data allowed to be returned by subquery
+- depending on the operator used in the WHERE clause ex: >, <, IN, NOT IN, etc.
+- Most the time you can solve the same problem with a JOIN clause
+- But sometimes a subquery in the WHERE clause is more readable
+ */
 --
 --
 --
-
-
-
-
--- Subquery in WHERE clause returns a single column ---------------------------------------------
-SELECT id
-FROM orders
-WHERE product_id IN (
-    SELECT id
-    FROM products
-    WHERE price / weight > 50
-  );
--- The same query using a JOIN clause -----------------------------------
-SELECT orders.id AS order_id
-FROM orders
-  JOIN products ON products.id = orders.product_id
-WHERE products.price / products.weight > 50;
-
-
-
-
-
--- Subquery in WHERE clause returns a single value ---------------------------------------------
-SELECT name
-FROM products
-WHERE price > (
-    SELECT AVG(price)
-    FROM products
-  );
+-- Subquery in WHERE clause returns a single column into the IN operator ---------------------------------------------
+SELECT
+    id
+FROM
+    orders
+WHERE
+    product_id IN (
+        SELECT
+            id
+        FROM
+            products
+        WHERE
+            price / weight > 50
+    );
 
 -- The same query using a JOIN clause -----------------------------------
-SELECT name
-FROM products
-  JOIN (
-    SELECT AVG(price) AS avg_price
-    FROM products
-  ) as p ON products.price > p.avg_price;
+SELECT
+    orders.id AS order_id
+FROM
+    orders
+    JOIN products ON products.id = orders.product_id
+WHERE
+    products.price / products.weight > 50;
+
+-- Subquery in WHERE clause returns a single value using > operator ---------------------------------------------
+SELECT
+    name
+FROM
+    products
+WHERE
+    price > (
+        SELECT
+            AVG(price)
+        FROM
+            products
+    );
+
+-- The same query using a JOIN clause -----------------------------------
+SELECT
+    name
+FROM
+    products
+    JOIN (
+        SELECT
+            AVG(price) AS avg_price
+        FROM
+            products
+    ) as p ON products.price > p.avg_price;
+
+-- Subquery in WHERE clause utilizing NOT IN operator ---------------------------------------------
+SELECT
+    name,
+    department
+FROM
+    products
+WHERE
+    department NOT IN (
+        SELECT
+            department
+        FROM
+            products
+        WHERE
+            price < 100
+    );
+
+-- Subquery in WHERE clause using > ALL operator -------------------------
+SELECT
+    name,
+    department,
+    price
+FROM
+    products
+WHERE
+    price > ALL (
+        SELECT
+            price
+        FROM
+            products
+        WHERE
+            department = 'Industrial'
+    );
